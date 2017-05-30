cc48dc5292f70f0e4e26790e9f429ff2bd873c55
HIR0Y0SHI <bardiche.assault7@gmail.com>
2017-05-30 17:08:19 +0900

    first commit🐣

---
+++ README.md
@@ -0,0 +1,9 @@
+# とべマスター
+
+## 構成
+* silex/silex: ~2.0
+* smarty/smarty": ~3.1
+
+
+## その他
+* ルーティングはweb/index.htmlで行う
---
+++ src/settings.php
@@ -0,0 +1,40 @@
+<?php
+return [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+
+        // Renderer settings
+        'renderer' => [
+            'template_path' => __DIR__ . '/../templates/',
+        ],
+
+        // Monolog settings
+        // 'logger' => [
+        //     'name' => 'slim-app',
+        //     'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+        //     'level' => \Monolog\Logger::DEBUG,
+        // ],
+
+
+        // View settings
+         'view' => [
+            'template_path' =>  __DIR__ . '/../templates/',
+            'twig' => [
+                'cache' =>  __DIR__ . '/../cache/',
+                'debug' => true,
+                'auto_reload' => true,
+            ],
+        ],
+
+
+        // DataBase(MySQL) settings
+        'db' => [
+            'host' => 'localhost',
+            'port' => '3306',
+            'user' => 'tobe',
+            'pass' => 'tobe',
+            'dbname' => 'tobe_master',
+        ],
+    ],
+];
