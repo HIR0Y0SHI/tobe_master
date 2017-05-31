043a13877d5571a37d7da9e0517eb01c1b46e672
HIR0Y0SHI <bardiche.assault7@gmail.com>
2017-05-31 11:19:31 +0900

    モックの追加

--- src/routes.php
+++ src/routes.php
@@ -16,3 +16,45 @@ $app->get('/api/question/{number}', function($request, $response, $args) {
     return $response;
 });
 
+
+//Mock
+// 一人で遊ぶ
+$app->get('/api/mock/question/one/{number}', function($request, $response, $args) {
+
+    $number = $args['number'];
+
+    $question = 'この動物はカピパラ？';
+
+    $json = [
+        'pattern' => 'A',
+        'question' => mb_convert_encoding($question, "UTF-8", "auto"),
+        'choices' => [
+            'ans' => 'Yes',
+            'inc' => 'No'
+        ],
+        'image' => 'sample.png'
+    ];
+
+    echo json_encode($json, JSON_UNESCAPED_UNICODE);
+
+});
+
+// 複数で遊ぶ（画像２枚の解答）
+$app->get('/api/mock/question/multiple/{number}', function($request, $response, $args) {
+
+    $number = $args['number'];
+
+    $question = 'カピパラはどっち？';
+
+    $json = [
+        'pattern' => 'B',
+        'question' => mb_convert_encoding($question, "UTF-8", "auto"),
+        'choices' => [
+            'ans' => 'sample.png',
+            'inc' => 'sample.png'
+        ]
+    ];
+
+    echo json_encode($json, JSON_UNESCAPED_UNICODE);
+
+});
