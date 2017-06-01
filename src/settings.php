843b0941a468ea748c753321b07152e5a51fbde6
HIR0Y0SHI <bardiche.assault7@gmail.com>
2017-06-01 14:11:50 +0900

    appディレクトリの追加🌝

--- src/settings.php
+++ src/settings.php
@@ -8,14 +8,7 @@ return [
         'renderer' => [
             'template_path' => __DIR__ . '/../templates/',
         ],
-
-        // Monolog settings
-        // 'logger' => [
-        //     'name' => 'slim-app',
-        //     'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
-        //     'level' => \Monolog\Logger::DEBUG,
-        // ],
-
+        
 
         // View settings
          'view' => [
