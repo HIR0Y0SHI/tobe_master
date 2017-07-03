3fd8a4410a81e7679ed6bc63274803f61887d88e
HIR0Y0SHI <bardiche.assault7@gmail.com>
2017-07-03 14:06:04 +0900

    上げ忘れ

---
+++ app/controllers/MakeAreaController.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by KUWANO on 2017/06/29
+ */
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\Area;
+
+class MakeAreaController extends BaseController {
+    
+    /**
+     * エリアを追加する
+     *
+     * @access public
+     * @param array $params
+     */
+    public function addArea($params) {
+
+        echo '<pre>';
+        //var_dump($bh);
+        var_dump($params);
+        echo '</pre>';
+
+        exit;
+    }
+
+    /**
+     * エリアを更新する
+     *
+     * @access public
+     * @param array $params
+     */
+    public function updateArea($params) {
+
+        //$this->app->view->render($this->response, 'management/makeQuestion.html');
+        echo '<pre>';
+        //var_dump($bh);
+        var_dump($params);
+        echo '</pre>';
+        exit;
+
+    }
+
+    /**
+     * エリアを削除する
+     *
+     * @access public
+     * @param array $params
+     */
+    public function deleteArea($params) {
+
+        echo '<pre>';
+        //var_dump($bh);
+        var_dump($params);
+        echo '</pre>';
+        exit;
+
+    }
+
+}
