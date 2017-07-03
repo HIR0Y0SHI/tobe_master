b4347926ec6e5ee02423684fa9b26693bd276a6f
桑野 広翔 <xhirox1023@gmail.com>
2017-07-03 11:41:06 +0900

    エリア関連のソース関連

---
+++ app/models/Area.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by HIR0Y0SHI on 2017/06/05
+ */
+
+namespace App\Models;
+
+use App\Mapper;
+
+/**
+ * エリア関連のDBアクセスクラス
+ *
+ * @author KUWANO
+ * @package Models
+ */
+class Area extends Mapper {
+
+    /**
+     * エリアを登録する
+     *
+     * @access public
+     * @param string $prams 入力パラメータ
+     * @return bool trueなら成功
+     */
+    public function registration($prams) {
+        $result = null;
+        return $result;
+    }
+    /**
+     * エリアを更新する
+     *
+     * @access public
+     * @param string $prams 入力パラメータ
+     * @return bool trueなら成功
+     */
+    public function update($prams) {
+        $result = null;
+        return $result;
+    }
+    /**
+     * エリアを削除する
+     *
+     * @access public
+     * @param string $prams 入力パラメータ
+     * @return bool trueなら成功
+     */
+    public function delete($prams) {
+        $result = null;
+        return $result;
+    }
+}
