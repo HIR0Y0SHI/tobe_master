cd9d488eed3c08d1abbfabeff75ee6c38796c353
HIR0Y0SHI <bardiche.assault7@gmail.com>
2017-07-09 01:45:40 +0900

    sessionによるログイン管理の実装

--- app/Auth.class.php
+++ app/Auth.class.php
@@ -5,6 +5,10 @@
 
 namespace App;
 
+/**
+* ログイン関連の認証を管理する
+*
+*/
 class Auth {
 
     // public function __construct() {
@@ -13,23 +17,44 @@ class Auth {
 
     public static function check($app, $response) {
         
-        if (!isset($_SESSION['user'])) {
+        // tobe_master Sessionの取得
+        $session = new \SlimSession\Helper;
+
+        // ログインされているかの確認
+        if (!$session->exists('user')) {
             // return $app->view->render($response, 'management/login.html');
             // return $response->withRedirect($app->router->pathFor('login'));
 
+            // MEMO: routerで実行すると、リダイレクトされるが、ここで実行しても動かない
+            // $url = $app->router->pathFor('login');    
+            // return $response->withStatus(302)->withHeader('Location', $url);
+
             // Redirectの処理がうまく動かないので強制
-            // header('Location: /tobe_master/login');
-            // exit;
+            header('Location: /tobe_master/login');
+            exit;
         }
     }
 
 
+
+    /**
+    * ログイン時に実行する。
+    * ログインするユーザー名をbase64に暗号化し,
+    * tobe_masterのSessionに保存する。
+    */
     public static function login($user) {
-        $_SESSION['user'] = base64_encode($user);
+        $session = new \SlimSession\Helper;
+        $session->user = base64_encode($user);
     }
 
 
+
+    /**
+    * ログアウト時に実行する。
+    * tobe_masterのSessionを破棄する。
+    */
     public static function logout() {
-        unset($_SESSION);
+        $session = new \SlimSession\Helper;
+        $session::destroy();
     }
 }
